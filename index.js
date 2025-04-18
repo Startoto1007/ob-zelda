5e12323b6fa8073c486dc928988846f06d5ccacc
Startoto1007 <gouessestar@gmail.com>
2025-04-18 10:43:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import 'dotenv/config';
 import { Client, GatewayIntentBits } from 'discord.js';
+import memberJoin from './events/memberJoin.js'; // Importation de l'événement
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] // On ajoute l'intent pour les membres
 });
 
 client.once('ready', () => {
   console.log(`🤖 Bot en ligne : ${client.user.tag}`);
 });
 
-// Tu pourras ajouter tes futures commandes ici
+// Enregistrement de l'événement de bienvenue
+memberJoin(client);
 
 client.login(process.env.DISCORD_TOKEN);
