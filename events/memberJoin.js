b04a2f8f552c0afd4bdc56e5c4ddaf5c423a90d1
Startoto1007 <gouessestar@gmail.com>
2025-04-19 14:49:29 +0200

    Update memberJoin.js

--- events/memberJoin.js
+++ events/memberJoin.js
@@ -8,16 +8,16 @@ export default (client) => {
       const height = 640;
       const canvas = createCanvas(width, height);
       const ctx = canvas.getContext('2d');
-
+      
       // Chargement de l'image de fond
       const background = await loadImage("https://res.cloudinary.com/dor9octmp/image/upload/v1745054283/Capture_d_e%CC%81cran_2025-04-19_a%CC%80_11.12.48_pclp9y.png");
       ctx.drawImage(background, 0, 0, width, height);
-
+      
       // Dimensions de l'avatar
-      const avatarSize = Math.floor(height * 2 / 5); // 2/5 de la hauteur de l’image
+      const avatarSize = Math.floor(height * 2 / 5); // 2/5 de la hauteur de l'image
       const avatarX = (width / 3) / 2 - avatarSize / 2; // centré dans le premier tiers horizontal
       const avatarY = height / 2 - avatarSize / 2;
-
+      
       // Chargement et affichage de l'avatar
       const avatar = await loadImage(member.user.displayAvatarURL({ extension: 'png', size: 512 }));
       ctx.save();
@@ -27,10 +27,10 @@ export default (client) => {
       ctx.clip();
       ctx.drawImage(avatar, avatarX, avatarY, avatarSize, avatarSize);
       ctx.restore();
-
+      
       const buffer = canvas.toBuffer('image/png');
       const attachment = new AttachmentBuilder(buffer, { name: 'welcome-image.png' });
-
+      
       // Embed en DM
       const welcomeEmbed = new EmbedBuilder()
         .setColor("#f500c0")
@@ -38,13 +38,13 @@ export default (client) => {
         .setDescription("Ici tu trouveras :\n • des concours\n • un super bot qui te donne les prérequis pour les prestiges\n • un salon de commerce\n • toutes les actualités de l'ob")
         .setImage("https://i.imgur.com/4ug9AH9.jpeg")
         .setTimestamp();
-
+      
       try {
         await member.send({ embeds: [welcomeEmbed] });
       } catch (error) {
         console.error("Impossible d'envoyer un DM à l'utilisateur :", error);
       }
-
+      
       const channel = member.guild.channels.cache.get("1348227800355569707");
       if (channel) {
         await channel.send({
@@ -55,7 +55,7 @@ export default (client) => {
         console.error("Canal introuvable !");
       }
     } catch (error) {
-      console.error("Erreur lors de l’envoi de l’image de bienvenue :", error);
+      console.error("Erreur lors de l'envoi de l'image de bienvenue :", error);
     }
   });
 };
