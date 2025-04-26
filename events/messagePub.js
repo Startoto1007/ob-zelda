86f3ecf122874cb796b0a9a7c4b21c8d56576b02
Startoto1007 <gouessestar@gmail.com>
2025-04-26 10:46:24 +0200

    Create messagePub.js

---
+++ events/messagePub.js
@@ -0,0 +1,35 @@
+import { EmbedBuilder } from 'discord.js';
+
+export const handleMessageCreate = async (message) => {
+  if (message.channel.id === '1365354306798354536' && !message.author.bot) {
+    const member = message.guild.members.cache.get(message.author.id);
+    const hasVerifiedRole = member.roles.cache.has('1339323401503903785');
+    const today = new Date().toISOString().split('T')[0];
+    const lastMessage = member.lastMessageSent || {};
+
+    if (!hasVerifiedRole) {
+      await message.delete();
+      const errorEmbed = new EmbedBuilder()
+        .setTitle('Erreur')
+        .setDescription('Vous n\'avez pas le rôle "Compte vérifié" requis pour poster dans ce salon. Veuillez ouvrir un ticket dans le salon <#1339335025161928724> pour l\'obtenir.')
+        .setColor(0xff0000);
+      await message.author.send({ embeds: [errorEmbed], ephemeral: true });
+      return;
+    }
+
+    if (lastMessage.channelId === message.channel.id && lastMessage.date === today) {
+      await message.delete();
+      const errorEmbed = new EmbedBuilder()
+        .setTitle('Erreur')
+        .setDescription('Vous avez déjà posté un message de pub aujourd\'hui. Veuillez attendre demain pour reposter votre pub.')
+        .setColor(0xff0000);
+      await message.author.send({ embeds: [errorEmbed], ephemeral: true });
+      return;
+    }
+
+    member.lastMessageSent = {
+      channelId: message.channel.id,
+      date: today,
+    };
+  }
+};
